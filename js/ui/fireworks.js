6814018427de9d5d2da1ba215645514efe4ebfd4
GloomyM <599238066@qq.com>
2021-01-17 21:27:08 +0800

    Site updated: 2021-01-17 21:26:16

--- js/ui/fireworks.js
+++ js/ui/fireworks.js
@@ -1,5 +1,7 @@
 // https://codepen.io/juliangarnier/pen/gmOwJX
-// custom by hexo-theme-yun
+// custom by hexo-theme-yun @YunYouJun
+
+// global CONFIG
 
 const numberOfParticules = 20;
 
@@ -15,21 +17,30 @@ const maxAnimeDuration = 1500;
 const minDiffuseRadius = 50;
 const maxDiffuseRadius = 100;
 
-let canvasEl = document.querySelector(".fireworks");
-let ctx = canvasEl.getContext("2d");
 let pointerX = 0;
 let pointerY = 0;
 
 // sky blue
 let colors = ["102, 167, 221", "62, 131, 225", "33, 78, 194"];
-if (CONFIG.fireworks.colors) colors = CONFIG.fireworks.colors;
+if (CONFIG.fireworks.colors) {
+  colors = CONFIG.fireworks.colors;
+}
 
-function setCanvasSize() {
+const canvasEl = document.querySelector(".fireworks");
+// global ctx
+const ctx = canvasEl.getContext("2d");
+
+/**
+ * 设置画布尺寸
+ */
+function setCanvasSize(canvasEl) {
   canvasEl.width = window.innerWidth;
   canvasEl.height = window.innerHeight;
   canvasEl.style.width = window.innerWidth + "px";
   canvasEl.style.height = window.innerHeight + "px";
 }
+
+setCanvasSize(canvasEl);
 
 /**
  * update pointer
@@ -111,10 +122,10 @@ function animateParticules(x, y) {
     .timeline()
     .add({
       targets: particules,
-      x: function(p) {
+      x(p) {
         return p.endPos.x;
       },
-      y: function(p) {
+      y(p) {
         return p.endPos.y;
       },
       radius: 0.1,
@@ -122,38 +133,42 @@ function animateParticules(x, y) {
       easing: "easeOutExpo",
       update: renderParticule,
     })
-    .add({
-      targets: circle,
-      radius: anime.random(minOrbitRadius, maxOrbitRadius),
-      lineWidth: 0,
-      alpha: {
-        value: 0,
-        easing: "linear",
-        duration: anime.random(600, 800),
+    .add(
+      {
+        targets: circle,
+        radius: anime.random(minOrbitRadius, maxOrbitRadius),
+        lineWidth: 0,
+        alpha: {
+          value: 0,
+          easing: "linear",
+          duration: anime.random(600, 800),
+        },
+        duration: anime.random(1200, 1800),
+        easing: "easeOutExpo",
+        update: renderParticule,
       },
-      duration: anime.random(1200, 1800),
-      easing: "easeOutExpo",
-      update: renderParticule,
-      offset: 0,
-    });
+      0
+    );
 }
 
-let render = anime({
-  duration: Infinity,
-  update: () => {
-    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-  },
+document.addEventListener("DOMContentLoaded", () => {
+  /* global anime */
+  const render = anime({
+    duration: Infinity,
+    update: () => {
+      ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    },
+  });
+
+  document.addEventListener(
+    "mousedown",
+    (e) => {
+      render.play();
+      updateCoords(e);
+      animateParticules(pointerX, pointerY);
+    },
+    false
+  );
 });
 
-document.addEventListener(
-  "mousedown",
-  (e) => {
-    render.play();
-    updateCoords(e);
-    animateParticules(pointerX, pointerY);
-  },
-  false
-);
-
-setCanvasSize();
-window.addEventListener("resize", setCanvasSize, false);
+window.addEventListener("resize", setCanvasSize(canvasEl), false);
